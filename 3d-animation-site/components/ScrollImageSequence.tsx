4a2f3e84dc4e20e196bfe817cd1c2125bf3074cc
viratcore01 <viratcore01@gmail.com>
2026-04-25 20:37:14 +0530

    fix: remove unused useEffect lint

--- 3d-animation-site/components/ScrollImageSequence.tsx
+++ 3d-animation-site/components/ScrollImageSequence.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion'
 
 interface ScrollImageProps {
---
+++ fix3.bat
@@ -0,0 +1,4 @@
+cd c:\Users\hp\Desktop\cake
+git add .
+git commit -m "fix: remove unused useEffect lint"
+git push -u origin main
